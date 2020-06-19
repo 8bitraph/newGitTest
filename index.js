02fd190dedee0e75443e15ed3a96b64ec1350e91
8bitraph <52341541+8bitraph@users.noreply.github.com>
2020-06-19 06:20:30 -0400

    tarted a new js course

---
+++ index.js
@@ -0,0 +1,15 @@
+// comment
+console.log("hello world");
+
+let name = "bit"; 
+console.log(name);
+
+// cannot be a reserved keyword
+// should be meanibgful
+// should not have a number
+// cannot contain a space or hypen
+// case sensitive 
+
+let firstName = "Nihao"; 
+
+let firstName = "aye", lastName = "no"; 
